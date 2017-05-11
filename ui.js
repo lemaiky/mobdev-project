3140a8da6024ab497387da45514fd1ae17c386ab
Jonathan Adam <jonathanadamuni@gmail.com>
2017-05-11 14:45:45 +0200

    Made some progress

---
+++ ui.js
@@ -0,0 +1,155 @@
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	console.log("YAAHAHERWQEORAHQE");
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	
+	//show the next fieldset
+	next_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'transform': 'scale('+scale+')'});
+			next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".submit").click(function(){
+	return false;
+})
+
+
+
+// (function()
+// {
+
+// 	//exclude older browsers by the features we need them to support
+// 	//and legacy opera explicitly so we don't waste time on a dead browser
+// 	if
+// 	(
+// 		!document.querySelectorAll 
+// 		|| 
+// 		!('draggable' in document.createElement('span')) 
+// 		|| 
+// 		window.opera
+// 	) 
+// 	{ return; }
+	
+// 	//get the collection of draggable items and add their draggable attribute
+// 	for(var 
+// 		items = document.querySelectorAll('[data-draggable="item"]'), 
+// 		len = items.length, 
+// 		i = 0; i < len; i ++)
+// 	{
+// 		items[i].setAttribute('draggable', 'true');
+// 	}
+
+// 	//variable for storing the dragging item reference 
+// 	//this will avoid the need to define any transfer data 
+// 	//which means that the elements don't need to have IDs 
+// 	var item = null;
+
+// 	//dragstart event to initiate mouse dragging
+// 	document.addEventListener('dragstart', function(e)
+// 	{
+// 		//set the item reference to this element
+// 		item = e.target;
+		
+// 		//we don't need the transfer data, but we have to define something
+// 		//otherwise the drop action won't work at all in firefox
+// 		//most browsers support the proper mime-type syntax, eg. "text/plain"
+// 		//but we have to use this incorrect syntax for the benefit of IE10+
+// 		e.dataTransfer.setData('text', '');
+	
+// 	}, false);
+
+// 	//dragover event to allow the drag by preventing its default
+// 	//ie. the default action of an element is not to allow dragging 
+// 	document.addEventListener('dragover', function(e)
+// 	{
+// 		if(item)
+// 		{
+// 			e.preventDefault();
+// 		}
+	
+// 	}, false);  
+
+// 	//drop event to allow the element to be dropped into valid targets
+// 	document.addEventListener('drop', function(e)
+// 	{
+// 		//if this element is a drop target, move the item here 
+// 		//then prevent default to allow the action (same as dragover)
+// 		if(e.target.getAttribute('data-draggable') == 'target')
+// 		{
+// 			e.target.appendChild(item);
+			
+// 			e.preventDefault();
+// 		}
+	
+// 	}, false);
+	
+// 	//dragend event to clean-up after drop or abort
+// 	//which fires whether or not the drop target was valid
+// 	document.addEventListener('dragend', function(e)
+// 	{
+// 		item = null;
+	
+// 	}, false);
+
+// })();   
