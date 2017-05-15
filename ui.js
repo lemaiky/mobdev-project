8fd63bb11ec1a840a02ebf27d4b0876b925575f8
Anders <Flanders@n177-p41.kthopen.kth.se>
2017-05-15 22:24:52 +0200

    STUFF

--- ui.js
+++ ui.js
@@ -910,9 +910,18 @@ function posnLoop(){
 }
 
 function removeFlag(teamId,flagId){
-	for (var i = 0; i < enemyFlagListUI.length; i++){
-		if (flagId === i){
-			enemyFlagListUI[i].setMap(null);
+	if (player.teamId == teamId){
+		for (var i = 0; i < ownFlagListUI.length; i++){
+			if (flagId === i){
+				ownFlagListUI[i].setMap(null);
+			}
+		}
+	}
+	} else {
+		for (var i = 0; i < enemyFlagListUI.length; i++){
+			if (flagId === i){
+				enemyFlagListUI[i].setMap(null);
+			}
 		}
 	}
 }
